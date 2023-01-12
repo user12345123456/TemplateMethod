bfb1839dbf8336a7b8b041ff384d6670b8b30447
h <angelinacom157@gmail.com>
2023-01-12 15:23:10 +0300

    Initial commit

---
+++ src/TemplatePattern.java
@@ -0,0 +1,11 @@
+public class TemplatePattern {
+    public static void main(String[] args) {
+
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
+
